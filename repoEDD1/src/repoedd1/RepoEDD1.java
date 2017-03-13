cde4922f208d5814c20191fb267c25ab1a7cbcd6
alex <alex@alex-EasyNote-SJ51>
2017-03-13 09:50:26 +0100

    pujant  canvi 1 netbeans

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -15,6 +15,7 @@ public class RepoEDD1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("IDEA ET EAS");
         
     }
     
